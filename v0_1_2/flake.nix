5811ca8158f1ae0cf4a6167ddd9f3175acd59fe0
hugosenari <hugosenari@gmail.com>
2022-03-27 21:36:21 -0300

    chore: re index nimqbittorrent

--- v0_1_2/flake.nix
+++ v0_1_2/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-nimQt-v0_1_2.ref   = "refs/tags/v0.1.2";
   inputs.src-nimQt-v0_1_2.owner = "faulander";
   inputs.src-nimQt-v0_1_2.repo  = "nimQBittorrent";
+  inputs.src-nimQt-v0_1_2.dir   = "";
   inputs.src-nimQt-v0_1_2.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
